2da1ada8f678d6794fc2d968a5dcc38dec2927bb
cyber-chalk <luka.chalker@gmail.com>
2022-11-03 08:41:30 +1100

    updated cookies

--- cookies.js
+++ cookies.js
@@ -3,13 +3,13 @@ let settings = document.getElementsByClassName("cookie-container")[0];
 const value = ("; " + document.cookie).split(`; name=`).pop().split(";")[0];
 
 //check if the user has already visited
-if (document.cookie == String) settings.style.display = "none";
+if (value == String) settings.style.display = "none";
 
 let input = document.getElementById("popup-input");
 let close = document.getElementById("close");
 close.addEventListener("click", function () {
-	// if (input.value == "") settings.style.display = "none";
 	document.cookie = "name=" + input.value + ";max-age=86400;";
+
 	console.log(document.cookie, input.value);
 	settings.style.display = "none";
 });
@@ -30,3 +30,6 @@ let closeProfile = document.getElementById("close-profile");
 closeProfile.addEventListener("click", function () {
 	profile.style.display = "none";
 });
+
+let status = document.querySelector(".profile-contianer h1");
+if (value == String) status.innerHTMl = value;
--- index.js
+++ index.js
@@ -4,44 +4,39 @@ let moneyValue = document.getElementById("money");
 let dateValue = document.getElementById("date");
 let infoBox = document.getElementById("info-box");
 
+let rect = container.getBoundingClientRect();
+let time;
+let money;
+
 container.addEventListener("mouseleave", function () {
 	infoBox.style.display = "none";
 });
 
-container.addEventListener(
-	"mousemove",
-	 function (event) {
-		let rect = container.getBoundingClientRect();
+container.addEventListener("mousemove", function (event) {
+	let x = Math.round(((event.clientX - rect.left) / rect.width) * 100);
+	let y = Math.round(((event.clientY - rect.top) / rect.height) * 100);
 
-		let x = Math.round(((event.clientX - rect.left) / rect.width) * 100);
-		let y = Math.round(((event.clientY - rect.top) / rect.height) * 100);
+	infoBox.style.display = "flex";
 
-		infoBox.style.display = "flex";
+	if (x >= 0 && x <= 10) time = "april";
+	else if (x >= 11 && x <= 25) time = "may";
+	else if (x >= 26 && x <= 40) time = "june";
+	else if (x >= 41 && x <= 55) time = "july";
+	else if (x >= 56 && x <= 69) time = "august";
+	else if (x >= 70 && x <= 88) time = "september";
+	else time = "No date";
 
-		let time;
-		let money;
+	if (y <= 90 && y >= 75) money = "$150";
+	else if (y <= 74 && y >= 60) money = "$200";
+	else if (y <= 59 && y >= 45) money = "$250";
+	else if (y <= 44 && y >= 28) money = "$300";
+	else if (y <= 27 && y >= 13) money = "$350";
+	else if (y <= 12 && y >= 1) money = "$400";
+	else money = "$0";
 
-		if (x >= 0 && x <= 10) time = "april";
-		else if (x >= 11 && x <= 25) time = "may";
-		else if (x >= 26 && x <= 40) time = "june";
-		else if (x >= 41 && x <= 55) time = "july";
-		else if (x >= 56 && x <= 69) time = "august";
-		else if (x >= 70 && x <= 88) time = "september";
-		else time = "No date";
+	moneyValue.innerHTML = `${money}`;
+	dateValue.innerHTML = `${time}`;
 
-		if (y <= 90 && y >= 75) money = "$150";
-		else if (y <= 74 && y >= 60) money = "$200";
-		else if (y <= 59 && y >= 45) money = "$250";
-		else if (y <= 44 && y >= 28) money = "$300";
-		else if (y <= 27 && y >= 13) money = "$350";
-		else if (y <= 12 && y >= 1) money = "$400";
-		else money = "$0";
-
-		moneyValue.innerHTML = `${money}`;
-		dateValue.innerHTML = `${time}`;
-
-		infoBox.style.top = event.clientY + "px";
-		infoBox.style.left = event.clientX + "px";
-	},
-	
-);
+	infoBox.style.top = event.clientY + "px";
+	infoBox.style.left = event.clientX + "px";
+});
